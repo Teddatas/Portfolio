7a3258c8b87f5b61aa0b5209dd9593e1845abb95
Teddatas <teddy.thiebault@gmail.com>
2022-09-15 22:40:59 +0200

    merci quentin

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $(".filter-menu").on('click', function(){
+        var value = $(this).attr('data-filter');
+        
+        if (value == "all") {
+            $('.filter').show('1000');
+        } else {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+    });
+
+});
